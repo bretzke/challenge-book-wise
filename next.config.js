d920b48fdea4324e5ab63fd718d471f0a23e8780
Willian Bretzke <willianbretzke2013@hotmail.com>
2023-08-20 18:22:31 -0300

    Inserido as imagens do livro

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
